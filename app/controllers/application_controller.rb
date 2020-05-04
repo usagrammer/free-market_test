b35e1be273e7be0f063d8f6bb8c48fdef67e7a11
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-05-04 13:20:23 +0900

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_paramaters, if: :devise_controller? ## 追加
+
+  private
+
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
+  def configure_permitted_paramaters ## 追加
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :first_name_reading, :last_name, :last_name_reading, :birthday])
+  end
+
 end
